fbab8d97df82683fc00bb22b98eb4ab69583b32d
Pierre Veelen (data-analytics) <pierre@pvln.nl>
2020-07-21 16:42:29 +0200

    conda commands added

--- install-ada.sh
+++ install-ada.sh
@@ -1,1 +1,54 @@
+#!/usr/bin/env bash
 
+# ---------------------------- INSTALL NOTES ----------------------------
+# To properly setup the AWS CLI, make sure that this shell script is run with the two required positional args:
+#   $1 :: (string) AWS Access Key ID
+#   $2 :: (string) AWS Secret Key
+#
+# Also make sure to give the script permission to run!:
+#    chmod 755 /home/ubuntu/install.sh
+#
+# -----------------------------------------------------------------------
+
+
+# ++++++++++++++++++++ START ANACONDA INSTALL +++++++++++++++++++++
+cd /home/ubuntu
+su ubuntu
+
+# Download the Linux Anaconda Distribution
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -O /tmp/anaconda3.sh
+
+# Run the installer (installing without -p should automatically install into '/' (root dir)
+bash /tmp/anaconda3.sh -b -p /home/ubuntu/anaconda3
+rm /tmp/anaconda3.sh
+
+### Run the conda init script to setup the shell
+echo ". /home/ubuntu/anaconda3/etc/profile.d/conda.sh" >> /home/ubuntu/.bashrc
+. /home/ubuntu/anaconda3/etc/profile.d/conda.sh
+source /home/ubuntu/.bashrc
+
+# Create base Python3 environments separate from the base env
+conda create -y --name ada38 python=3.8 
+conda create -y --name base38 python=3.8 
+
+# +++++++++++++++++++++ END ANACONDA INSTALL ++++++++++++++++++++++
+
+
+# ++++++++++++++ SETUP ENV +++++++++++++++
+
+# Install necessary Python packages
+# Note that 'source' is deprecated, so now we should be using 'conda' to activate/deactivate envs
+conda activate ada38
+#conda install -y -c conda-forge awscli 
+conda install -y notebook conda_kernels
+conda install -y geopandas
+conda install -y requests
+conda install -y mysql-connector-python
+conda install -y pysftp
+
+
+### Setup the credentials for the AWS CLI
+##aws configure set aws_access_key_id $1
+##aws configure set aws_secret_access_key $2
+
+# ++++++++++++ END  +++++++++++++
