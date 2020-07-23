4424d6edde568ac527e7ded7fef931857625d883
Pierre Veelen (data-analytics) <pierre@pvln.nl>
2020-07-23 10:34:49 +0200

    newer versions

--- install-ada.sh
+++ install-ada.sh
@@ -13,7 +13,7 @@
 
 # ++++++++++++++++++++ START ANACONDA INSTALL +++++++++++++++++++++
 cd /home/ubuntu
-su ubuntu
+#su ubuntu
 
 # Download the Linux Anaconda Distribution
 wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -O /tmp/anaconda3.sh
@@ -28,8 +28,9 @@ echo ". /home/ubuntu/anaconda3/etc/profile.d/conda.sh" >> /home/ubuntu/.bashrc
 source /home/ubuntu/.bashrc
 
 # Create base Python3 environments separate from the base env
-conda create -y --name ada38 python=3.8 
-conda create -y --name base38 python=3.8 
+conda update -n base -c defaults conda
+conda create -y --name ada37 python=3.7 
+conda create -y --name base37 python=3.7 
 
 # +++++++++++++++++++++ END ANACONDA INSTALL ++++++++++++++++++++++
 
@@ -38,7 +39,7 @@ conda create -y --name base38 python=3.8
 
 # Install necessary Python packages
 # Note that 'source' is deprecated, so now we should be using 'conda' to activate/deactivate envs
-conda activate ada38
+conda activate ada37
 #conda install -y -c conda-forge awscli 
 conda install -y notebook conda_kernels
 conda install -y geopandas
@@ -46,6 +47,8 @@ conda install -y requests
 conda install -y mysql-connector-python
 conda install -y pysftp
 
+#save current environment settings
+conda env export --no-builds > /home/ubuntu/environment.yml
 
 ### Setup the credentials for the AWS CLI
 ##aws configure set aws_access_key_id $1
