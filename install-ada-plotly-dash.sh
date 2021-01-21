0e00ce7ead18309e5bc111bdb662b68b9e72689b
Pierre Veelen (data-analytics) <pierre@pvln.nl>
2021-01-21 16:56:30 +0100

    script updated and typo fixed

--- install-ada-plotly-dash.sh
+++ install-ada-plotly-dash.sh
@@ -67,11 +67,15 @@ mkdir code
 mkdir output
 
 cd /home/ubuntu
-wget http:/download.pvln.nl/python/fastfood/code/show_selection_and_point_on_map_dynamic.py -O /home/ubuntu/plotlydash/code/the_code.py
-wget http:/download.pvln.nl/python/fastfood/output/_FastFood.csv -O /home/ubuntu/plotlydash/output/_FastFood.csv
+wget http://download.pvln.nl/python/fastfood/code/show_selection_and_point_on_map_dynamic.py -O /home/ubuntu/plotlydash/code/the_code.py
+wget http://download.pvln.nl/python/fastfood/output/_FastFood.csv -O /home/ubuntu/plotlydash/output/_FastFood.csv
 
-echo "coda activate ada38" >/home/ubuntu/start.sh
-echo "python plotlydash/code/the_code.py" >>/home/ubuntu/start.sh
+echo "#/bin/bash" >/home/ubuntu/start.sh
+echo "# code assumes to be run from within plotlydash/code folder">>/home/ubuntu/start.sh
+echo "cd /home/ubuntu/plotlydash/code">>/home/ubuntu/start.sh
+echo "ls -la">>/home/ubuntu/start.sh
+echo "/home/ubuntu/anaconda3/envs/ada38/bin/python3 --version">>/home/ubuntu/start.sh
+echo "/home/ubuntu/anaconda3/envs/ada38/bin/python3 the_code.py">>/home/ubuntu/start.sh
 chmod +x start.sh
 
 # ++++++++++++ END  +++++++++++++
