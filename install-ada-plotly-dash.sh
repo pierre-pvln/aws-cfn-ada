a2c1589db143ed5681b313cf4ed76bc3256b4dcd
Pierre Veelen (data-analytics) <pierre@pvln.nl>
2021-01-21 15:50:49 +0100

    wget shortend

--- install-ada-plotly-dash.sh
+++ install-ada-plotly-dash.sh
@@ -67,8 +67,7 @@ mkdir code
 mkdir output
 
 cd /home/ubuntu
-wget http:/download.pvln.nl/python/fastfood/code/show_selection_and_point_on_map_dynamic_v02.py -O /home/ubuntu/plotlydash/code/show_selection_and_point_on_map_dynamic_v02.py
-cp /home/ubuntu/plotlydash/code/show_selection_and_point_on_map_dynamic_v02.py /home/ubuntu/plotlydash/code/the_code.py
+wget http:/download.pvln.nl/python/fastfood/code/show_selection_and_point_on_map_dynamic.py -O /home/ubuntu/plotlydash/code/the_code.py
 wget http:/download.pvln.nl/python/fastfood/output/_FastFood.csv -O /home/ubuntu/plotlydash/output/_FastFood.csv
 
 echo "coda activate ada38" >/home/ubuntu/start.sh
